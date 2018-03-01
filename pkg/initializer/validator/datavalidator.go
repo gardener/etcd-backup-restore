383b3316092fc739d7b483090a98bad00009d0e1
I308301 <george.kuruvilla@sap.com>
2018-03-01 12:41:49 +0530

    Changing data validation to validate directories and files inside instead of starting etcd using embed.StartEtcd().

--- pkg/initializer/validator/datavalidator.go
+++ pkg/initializer/validator/datavalidator.go
@@ -15,15 +15,23 @@
 package validator
 
 import (
+	"errors"
 	"fmt"
+	"hash/crc32"
 	"io"
+	"io/ioutil"
 	"os"
 	"path/filepath"
-	"time"
+	"sort"
+	"strings"
 
+	bolt "github.com/coreos/bbolt"
+	"github.com/coreos/etcd/raft/raftpb"
+	"github.com/coreos/etcd/snap"
+	"github.com/coreos/etcd/snap/snappb"
+	"github.com/coreos/etcd/wal"
+	"github.com/coreos/etcd/wal/walpb"
 	"github.com/sirupsen/logrus"
-
-	"github.com/coreos/etcd/embed"
 )
 
 const (
@@ -41,10 +49,31 @@ const (
 	DataDirectoryError
 )
 
+const (
+	snapSuffix = ".snap"
+)
+
+var (
+	// A map of valid files that can be present in the snap folder.
+	validFiles = map[string]bool{
+		"db": true,
+	}
+	crcTable = crc32.MakeTable(crc32.Castagnoli)
+
+	// ErrPathRequired is returned when the path to a Bolt database is not specified.
+	ErrPathRequired = errors.New("path required")
+
+	// ErrFileNotFound is returned when a Bolt database does not exist.
+	ErrFileNotFound = errors.New("file not found")
+
+	// ErrCorrupt is returned when a checking a data file finds errors.
+	ErrCorrupt = errors.New("invalid value")
+
+	logger *logrus.Logger
+)
+
 // DataDirStatus represents the status of the etcd data directory.
 type DataDirStatus int
-
-var logger *logrus.Logger
 
 func init() {
 	logger = logrus.New()
@@ -55,6 +84,8 @@ func (d *DataValidator) memberDir() string { return filepath.Join(d.Config.DataD
 func (d *DataValidator) walDir() string { return filepath.Join(d.memberDir(), "wal") }
 
 func (d *DataValidator) snapDir() string { return filepath.Join(d.memberDir(), "snap") }
+
+func (d *DataValidator) backendPath() string { return filepath.Join(d.snapDir(), "db") }
 
 //Validate performs the steps required to validate data for Etcd instance.
 // The steps involed are:
@@ -98,6 +129,26 @@ func (d *DataValidator) Validate() (DataDirStatus, error) {
 	return DataDirectoryEmpty, nil
 }
 
+func (d *DataValidator) checkForDataCorruption() error {
+	var walsnap walpb.Snapshot
+	fmt.Println("Verifying snap directory...")
+	snapshot, err := d.verifySnapDir()
+	if err != nil && err != snap.ErrNoSnapshot {
+		return err
+	}
+
+	if snapshot != nil {
+		walsnap.Index, walsnap.Term = snapshot.Metadata.Index, snapshot.Metadata.Term
+	}
+	fmt.Println("Verifying WAL directory...")
+	if err := verifyWALDir(d.walDir(), walsnap); err != nil {
+		return err
+	}
+	fmt.Println("Verifying DB file...")
+	err = verifyDB(d.backendPath())
+	return err
+}
+
 func (d *DataValidator) hasEtcdDirectoryStructure() (bool, error) {
 	var memberExist, snapExist, walExist bool
 	var err error
@@ -111,42 +162,6 @@ func (d *DataValidator) hasEtcdDirectoryStructure() (bool, error) {
 		return false, err
 	}
 	return memberExist && snapExist && walExist, nil
-}
-
-func (d *DataValidator) checkForDataCorruption() error {
-	etcd, err := startEmbeddedEtcd(d.Config.DataDir)
-	if err != nil {
-		err = fmt.Errorf("Corruption check error: %v", err)
-		return err
-	}
-	logger.Info("Embedded server is ready!")
-	defer etcd.Close()
-	return nil
-}
-
-func startEmbeddedEtcd(dataDir string) (e *embed.Etcd, err error) {
-	defer func() {
-		if r := recover(); r != nil {
-			err = fmt.Errorf("etcd server crashed while starting")
-		}
-	}()
-	cfg := embed.NewConfig()
-	cfg.Dir = dataDir
-	e, err = embed.StartEtcd(cfg)
-
-	if err != nil {
-		err = fmt.Errorf("Corruption check error: %v", err)
-		return e, err
-	}
-	select {
-	case <-e.Server.ReadyNotify():
-		logger.Info("Embedded server is ready!")
-	case <-time.After(60 * time.Second):
-		e.Server.Stop() // trigger a shutdown
-		e.Close()
-		return nil, fmt.Errorf("server took too long to start")
-	}
-	return e, err
 }
 
 func directoryExist(dir string) (bool, error) {
@@ -171,4 +186,165 @@ func isDirEmpty(dataDir string) (bool, error) {
 		return true, nil
 	}
 	return false, err // Either not empty or error, suits both cases
+}
+
+func (d *DataValidator) verifySnapDir() (*raftpb.Snapshot, error) {
+	names, err := d.snapNames()
+	if err != nil {
+		return nil, err
+	}
+
+	var snapshot *raftpb.Snapshot
+	for _, name := range names {
+		if snapshot, err = verifySnap(d.snapDir(), name); err == nil {
+			break
+		}
+	}
+	if err != nil {
+		return nil, snap.ErrNoSnapshot
+	}
+	return snapshot, nil
+}
+
+func verifySnap(dir, name string) (*raftpb.Snapshot, error) {
+	fpath := filepath.Join(dir, name)
+	snap, err := read(fpath)
+	return snap, err
+}
+
+// Read reads the snapshot named by snapname and returns the snapshot.
+func read(snapname string) (*raftpb.Snapshot, error) {
+	fmt.Printf("Verifying Snapfile %s.\n", snapname)
+	b, err := ioutil.ReadFile(snapname)
+	if err != nil {
+		fmt.Printf("Cannot read file %v: %v.\n", snapname, err)
+		return nil, err
+	}
+
+	if len(b) == 0 {
+		fmt.Printf("Unexpected empty snapshot.\n")
+		return nil, snap.ErrEmptySnapshot
+	}
+
+	var serializedSnap snappb.Snapshot
+	if err = serializedSnap.Unmarshal(b); err != nil {
+		fmt.Printf("Corrupted snapshot file %v: %v.\n", snapname, err)
+		return nil, err
+	}
+
+	if len(serializedSnap.Data) == 0 || serializedSnap.Crc == 0 {
+		fmt.Printf("Unexpected empty snapshot.\n")
+		return nil, snap.ErrEmptySnapshot
+	}
+
+	crc := crc32.Update(0, crcTable, serializedSnap.Data)
+	if crc != serializedSnap.Crc {
+		fmt.Printf("Corrupted snapshot file %v: crc mismatch.\n", snapname)
+		return nil, snap.ErrCRCMismatch
+	}
+
+	var snap raftpb.Snapshot
+	if err = snap.Unmarshal(serializedSnap.Data); err != nil {
+		fmt.Printf("corrupted snapshot file %v: %v", snapname, err)
+		return nil, err
+	}
+	return &snap, nil
+}
+
+func (d *DataValidator) snapNames() ([]string, error) {
+	dir, err := os.Open(d.snapDir())
+	if err != nil {
+		return nil, err
+	}
+	defer dir.Close()
+	names, err := dir.Readdirnames(-1)
+	if err != nil {
+		return nil, err
+	}
+	snaps := checkSuffix(names)
+	if len(snaps) == 0 {
+		return nil, snap.ErrNoSnapshot
+	}
+	sort.Sort(sort.Reverse(sort.StringSlice(snaps)))
+	return snaps, nil
+}
+
+func checkSuffix(names []string) []string {
+	snaps := []string{}
+	for i := range names {
+		if strings.HasSuffix(names[i], snapSuffix) {
+			snaps = append(snaps, names[i])
+		} else {
+			// If we find a file which is not a snapshot then check if it's
+			// a vaild file. If not throw out a warning.
+			if _, ok := validFiles[names[i]]; !ok {
+				fmt.Printf("skipped unexpected non snapshot file %v", names[i])
+			}
+		}
+	}
+	return snaps
+}
+
+func verifyWALDir(waldir string, snap walpb.Snapshot) error {
+	var (
+		err error
+		w   *wal.WAL
+	)
+
+	repaired := false
+	for {
+		if w, err = wal.Open(waldir, snap); err != nil {
+			fmt.Printf("open wal error: %v", err)
+		}
+		if _, _, _, err = w.ReadAll(); err != nil {
+			w.Close()
+			// we can only repair ErrUnexpectedEOF and we never repair twice.
+			if repaired || err != io.ErrUnexpectedEOF {
+				fmt.Printf("read wal error (%v) and cannot be repaired.\n", err)
+				return err
+			}
+			if !wal.Repair(waldir) {
+				fmt.Printf("WAL error (%v) cannot be repaired.\n", err)
+				return err
+			}
+			fmt.Printf("repaired WAL error (%v).\n", err)
+			repaired = true
+
+			continue
+		}
+		break
+	}
+	return err
+}
+
+func verifyDB(path string) error {
+
+	if path == "" {
+		return ErrPathRequired
+	} else if _, err := os.Stat(path); os.IsNotExist(err) {
+		return ErrFileNotFound
+	}
+
+	// Open database.
+	db, err := bolt.Open(path, 0666, nil)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	// Perform consistency check.
+	return db.View(func(tx *bolt.Tx) error {
+		var count int
+		for _ = range tx.Check() {
+			count++
+		}
+
+		// Print summary of errors.
+		if count > 0 {
+			return ErrCorrupt
+		}
+
+		// Notify user that database is valid.
+		return nil
+	})
 }
