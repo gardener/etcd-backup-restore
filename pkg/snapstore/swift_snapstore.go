c8ae58a3e328e6085d49ff7664cc8968c320e270
Ishan Tyagi <ishan.tyagi@sap.com>
2022-02-10 19:05:41 +0530

    Added support to fetch openstack storage credentials from file.

--- pkg/snapstore/swift_snapstore.go
+++ pkg/snapstore/swift_snapstore.go
@@ -16,6 +16,7 @@ package snapstore
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -37,7 +38,9 @@ import (
 )
 
 const (
-	envPrefixSource = "SOURCE_OS_"
+	envPrefixSource         = "SOURCE_OS_"
+	swiftCredentialFile     = "OPENSTACK_APPLICATION_CREDENTIALS"
+	swiftCredentialJSONFile = "OPENSTACK_APPLICATION_CREDENTIALS_JSON"
 )
 
 // SwiftSnapStore is snapstore with Openstack Swift as backend
@@ -50,6 +53,16 @@ type SwiftSnapStore struct {
 	tempDir                 string
 }
 
+type swiftCredentials struct {
+	AuthURL    string `json:"authURL"`
+	BucketName string `json:"bucketName"`
+	DomainName string `json:"domainName"`
+	Password   string `json:"password"`
+	Region     string `json:"region"`
+	TenantName string `json:"tenantName"`
+	Username   string `json:"username"`
+}
+
 const (
 	// Total number of chunks to be uploaded must be one less than maximum limit allowed.
 	swiftNoOfChunk int64 = 999 //Default configuration in swift installation
@@ -57,7 +70,12 @@ const (
 
 // NewSwiftSnapStore create new SwiftSnapStore from shared configuration with specified bucket
 func NewSwiftSnapStore(config *brtypes.SnapstoreConfig) (*SwiftSnapStore, error) {
-	authOpts, err := clientconfig.AuthOptions(getClientOpts(config.IsSource))
+	clientOpts, err := getClientOpts(config.IsSource)
+	if err != nil {
+		return nil, err
+	}
+
+	authOpts, err := clientconfig.AuthOptions(clientOpts)
 	if err != nil {
 		return nil, err
 	}
@@ -82,13 +100,123 @@ func NewSwiftSnapStore(config *brtypes.SnapstoreConfig) (*SwiftSnapStore, error)
 
 }
 
-func getClientOpts(isSource bool) *clientconfig.ClientOpts {
+func getClientOpts(isSource bool) (*clientconfig.ClientOpts, error) {
 	if isSource {
 		return &clientconfig.ClientOpts{
 			EnvPrefix: envPrefixSource,
+		}, nil
+	}
+
+	if _, isSet := os.LookupEnv(swiftCredentialJSONFile); isSet {
+		if filename := os.Getenv(swiftCredentialJSONFile); filename != "" {
+			clientOpts, err := readSwiftCredentialsJSON(filename)
+			if err != nil {
+				return &clientconfig.ClientOpts{}, fmt.Errorf("error getting credentials using %v file", filename)
+			}
+			return clientOpts, nil
 		}
 	}
-	return &clientconfig.ClientOpts{}
+
+	if _, isSet := os.LookupEnv(swiftCredentialFile); isSet {
+		if dir := os.Getenv(swiftCredentialFile); dir != "" {
+			clientOpts, err := readSwiftCredentialFiles(dir)
+			if err != nil {
+				return &clientconfig.ClientOpts{}, fmt.Errorf("error getting credentials from %v directory", dir)
+			}
+			return clientOpts, nil
+		}
+	}
+
+	return &clientconfig.ClientOpts{}, nil
+}
+
+func readSwiftCredentialsJSON(filename string) (*clientconfig.ClientOpts, error) {
+	jsonData, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	return swiftCredentialsFromJSON(jsonData)
+
+}
+
+// swiftCredentialsFromJSON obtains Swift credentials from a JSON value.
+func swiftCredentialsFromJSON(jsonData []byte) (*clientconfig.ClientOpts, error) {
+	cred := &swiftCredentials{}
+	if err := json.Unmarshal(jsonData, cred); err != nil {
+		return nil, err
+	}
+
+	os.Setenv("OS_TENANT_NAME", cred.TenantName)
+	return &clientconfig.ClientOpts{
+		AuthInfo: &clientconfig.AuthInfo{
+			AuthURL:    cred.AuthURL,
+			DomainName: cred.DomainName,
+			Password:   cred.Password,
+			Username:   cred.Username,
+		},
+		RegionName: cred.Region,
+	}, nil
+}
+
+func readSwiftCredentialFiles(dirname string) (*clientconfig.ClientOpts, error) {
+	cred := &swiftCredentials{}
+
+	files, err := ioutil.ReadDir(dirname)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, file := range files {
+		if file.Name() == "authURL" {
+			data, err := ioutil.ReadFile(dirname + "/authURL")
+			if err != nil {
+				return nil, err
+			}
+			cred.AuthURL = string(data)
+		} else if file.Name() == "domainName" {
+			data, err := ioutil.ReadFile(dirname + "/domainName")
+			if err != nil {
+				return nil, err
+			}
+			cred.DomainName = string(data)
+		} else if file.Name() == "password" {
+			data, err := ioutil.ReadFile(dirname + "/password")
+			if err != nil {
+				return nil, err
+			}
+			cred.Password = string(data)
+		} else if file.Name() == "region" {
+			data, err := ioutil.ReadFile(dirname + "/region")
+			if err != nil {
+				return nil, err
+			}
+			cred.Region = string(data)
+		} else if file.Name() == "tenantName" {
+			data, err := ioutil.ReadFile(dirname + "/tenantName")
+			if err != nil {
+				return nil, err
+			}
+			cred.TenantName = string(data)
+		} else if file.Name() == "username" {
+			data, err := ioutil.ReadFile(dirname + "/username")
+			if err != nil {
+				return nil, err
+			}
+			cred.Username = string(data)
+		}
+	}
+
+	os.Setenv("OS_TENANT_NAME", cred.TenantName)
+	return &clientconfig.ClientOpts{
+		AuthInfo: &clientconfig.AuthInfo{
+			AuthURL:    cred.AuthURL,
+			DomainName: cred.DomainName,
+			Password:   cred.Password,
+			Username:   cred.Username,
+		},
+		RegionName: cred.Region,
+	}, nil
 }
 
 // NewSwiftSnapstoreFromClient will create the new Swift snapstore object from Swift client
