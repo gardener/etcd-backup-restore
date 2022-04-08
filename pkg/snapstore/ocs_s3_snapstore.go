9d0bba7b8d6e25ff5b0804679715a263ac102d7f
Johann Gnaucke <johann.gnaucke@plusserver.com>
2022-04-08 19:07:07 +0200

    Added functionality to copy etcd backups between ocs buckets

--- pkg/snapstore/ocs_s3_snapstore.go
+++ pkg/snapstore/ocs_s3_snapstore.go
@@ -14,64 +14,170 @@
 
 package snapstore
 
-import brtypes "github.com/gardener/etcd-backup-restore/pkg/types"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+
+	brtypes "github.com/gardener/etcd-backup-restore/pkg/types"
+)
 
 const (
-	ocsDefaultDisableSSL         bool = false
-	ocsDefaultInsecureSkipVerify bool = false
-
-	ocsEndpoint           string = "OCS_ENDPOINT"
-	ocsRegion             string = "OCS_REGION"
-	ocsDisableSSL         string = "OCS_DISABLE_SSL"
-	ocsInsecureSkipVerify string = "OCS_INSECURE_SKIP_VERIFY"
-	ocsAccessKeyID        string = "OCS_ACCESS_KEY_ID"
-	ocsSecretAccessKey    string = "OCS_SECRET_ACCESS_KEY"
+	ocsCredentialFile         string = "OPENSHIFT_APPLICATION_CREDENTIALS"
+	ocsCredentialFileJSONFile string = "OPENSHIFT_APPLICATION_CREDENTIALS_JSON"
 )
+
+type ocsAuthOptions struct {
+	Endpoint           string `json:"endpoint"`
+	Region             string `json:"region"`
+	DisableSSL         bool   `json:"disableSSL"`
+	InsecureSkipVerify bool   `json:"insecureSkipVerify"`
+	AccessKeyID        string `json:"accessKeyID"`
+	SecretAccessKey    string `json:"secretAccessKey"`
+}
 
 // NewOCSSnapStore creates a new S3SnapStore from shared configuration with the specified bucket.
 func NewOCSSnapStore(config *brtypes.SnapstoreConfig) (*S3SnapStore, error) {
-	ao, err := ocsAuthOptionsFromEnv()
+	credentials, err := getOCSAuthOptions(getEnvPrefixString(config.IsSource))
 	if err != nil {
 		return nil, err
 	}
-	return newGenericS3FromAuthOpt(config.Container, config.Prefix, config.TempDir, config.MaxParallelChunkUploads, ao)
+
+	return newGenericS3FromAuthOpt(config.Container, config.Prefix, config.TempDir, config.MaxParallelChunkUploads, ocsAuthOptionsToGenericS3(*credentials))
 }
 
-// ocsAuthOptionsFromEnv gets OCS provider configuration from environment variables.
-func ocsAuthOptionsFromEnv() (s3AuthOptions, error) {
-	endpoint, err := GetEnvVarOrError(ocsEndpoint)
-	if err != nil {
-		return s3AuthOptions{}, err
-	}
-	accessKeyID, err := GetEnvVarOrError(ocsAccessKeyID)
-	if err != nil {
-		return s3AuthOptions{}, err
-	}
-	secretAccessKey, err := GetEnvVarOrError(ocsSecretAccessKey)
-	if err != nil {
-		return s3AuthOptions{}, err
-	}
-	region, err := GetEnvVarOrError(ocsRegion)
-	if err != nil {
-		return s3AuthOptions{}, err
-	}
-	disableSSL, err := GetEnvVarToBool(ocsDisableSSL)
-	if err != nil {
-		disableSSL = ocsDefaultDisableSSL
-	}
-	insecureSkipVerify, err := GetEnvVarToBool(ocsInsecureSkipVerify)
-	if err != nil {
-		insecureSkipVerify = ocsDefaultInsecureSkipVerify
+func getOCSAuthOptions(prefix string) (*ocsAuthOptions, error) {
+	if _, isSet := os.LookupEnv(prefix + ocsCredentialFileJSONFile); isSet {
+		if filename := os.Getenv(prefix + ocsCredentialFileJSONFile); filename != "" {
+			ao, err := readOCSCredentialsJSON(filename)
+			if err != nil {
+				return nil, fmt.Errorf("error getting credentials using %v file", filename)
+			}
+			return ao, nil
+		}
 	}
 
-	ao := s3AuthOptions{
-		endpoint:           endpoint,
-		region:             region,
-		disableSSL:         disableSSL,
-		insecureSkipVerify: insecureSkipVerify,
-		accessKeyID:        accessKeyID,
-		secretAccessKey:    secretAccessKey,
+	if _, isSet := os.LookupEnv(prefix + ocsCredentialFile); isSet {
+		if dir := os.Getenv(prefix + ocsCredentialFile); dir != "" {
+			ao, err := readOCSCredentialFromDir(dir)
+			if err != nil {
+				return nil, fmt.Errorf("error getting credentials from %v directory", dir)
+			}
+			return ao, nil
+		}
 	}
 
-	return ao, nil
+	return nil, fmt.Errorf("unable to get credentials")
+}
+
+func readOCSCredentialFromDir(dirname string) (*ocsAuthOptions, error) {
+	ao := ocsAuthOptions{}
+
+	files, err := ioutil.ReadDir(dirname)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, file := range files {
+		switch file.Name() {
+		case "endpoint":
+			{
+				data, err := ioutil.ReadFile(dirname + "/endpoint")
+				if err != nil {
+					return nil, err
+				}
+				ao.Endpoint = string(data)
+			}
+		case "accessKeyID":
+			{
+				data, err := ioutil.ReadFile(dirname + "/accessKeyID")
+				if err != nil {
+					return nil, err
+				}
+				ao.AccessKeyID = string(data)
+			}
+		case "secretAccessKey":
+			{
+				data, err := ioutil.ReadFile(dirname + "/secretAccessKey")
+				if err != nil {
+					return nil, err
+				}
+				ao.SecretAccessKey = string(data)
+			}
+		case "region":
+			{
+				data, err := ioutil.ReadFile(dirname + "/region")
+				if err != nil {
+					return nil, err
+				}
+				ao.Region = string(data)
+			}
+		case "disableSSL":
+			{
+				data, err := ioutil.ReadFile(dirname + "/disableSSL")
+				if err != nil {
+					return nil, err
+				}
+				ao.DisableSSL, err = strconv.ParseBool(string(data))
+				if err != nil {
+					return nil, err
+				}
+			}
+		case "insecureSkipVerify":
+			{
+				data, err := ioutil.ReadFile(dirname + "/insecureSkipVerify")
+				if err != nil {
+					return nil, err
+				}
+				ao.InsecureSkipVerify, err = strconv.ParseBool(string(data))
+				if err != nil {
+					return nil, err
+				}
+			}
+		}
+	}
+
+	if err := isOCSConfigEmpty(ao); err != nil {
+		return nil, err
+	}
+	return &ao, nil
+}
+
+func readOCSCredentialsJSON(filename string) (*ocsAuthOptions, error) {
+	jsonData, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	return ocsCredentialsFromJSON(jsonData)
+}
+
+// ocsCredentialsFromJSON obtains OCS credentials from a JSON value.
+func ocsCredentialsFromJSON(jsonData []byte) (*ocsAuthOptions, error) {
+	ocsConfig := ocsAuthOptions{}
+	if err := json.Unmarshal(jsonData, &ocsConfig); err != nil {
+		return nil, err
+	}
+
+	return &ocsConfig, nil
+}
+
+func ocsAuthOptionsToGenericS3(options ocsAuthOptions) s3AuthOptions {
+	return s3AuthOptions{
+		endpoint:           options.Endpoint,
+		region:             options.Region,
+		accessKeyID:        options.AccessKeyID,
+		secretAccessKey:    options.SecretAccessKey,
+		disableSSL:         options.DisableSSL,
+		insecureSkipVerify: options.InsecureSkipVerify,
+	}
+}
+
+func isOCSConfigEmpty(config ocsAuthOptions) error {
+	if len(config.AccessKeyID) != 0 && len(config.Region) != 0 && len(config.SecretAccessKey) != 0 && len(config.Endpoint) != 0 {
+		return nil
+	}
+	return fmt.Errorf("ocs s3 credentials: region, secretAccessKey, endpoint or accessKeyID is missing")
 }
