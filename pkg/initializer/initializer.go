4580150b1d71a40120f205c7a771bf665e8bdc0c
Ishan Tyagi <42602577+ishan16696@users.noreply.github.com>
2023-08-02 11:56:55 +0530

    Skip scale-up checks for first member of etcd cluster. (#649)
    * Skip scale-up checks for first member of etcd cluster.
    
    * Add a TODO comment.

--- pkg/initializer/initializer.go
+++ pkg/initializer/initializer.go
@@ -19,6 +19,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/gardener/etcd-backup-restore/pkg/errors"
@@ -57,8 +58,15 @@ func (e *EtcdInitializer) Initialize(mode validator.Mode, failBelowRevision int6
 	ctx := context.Background()
 	var err error
 
-	//Etcd cluster scale-up case
-	if miscellaneous.IsMultiNode(logger) {
+	podName, err := miscellaneous.GetEnvVarOrError("POD_NAME")
+	if err != nil {
+		logger.Fatalf("Error reading POD_NAME env var : %v", err)
+	}
+
+	// Etcd cluster scale-up case
+	// Note: first member of etcd cluster can never be part of scale-up case.
+	// TODO: consider removing this special check for first cluster member when backup-restore can check presence of any member in cluster.
+	if miscellaneous.IsMultiNode(logger) && !strings.HasSuffix(podName, "0") {
 		clientSet, err := miscellaneous.GetKubernetesClientSetOrError()
 		if err != nil {
 			logger.Fatalf("failed to create clientset, %v", err)
