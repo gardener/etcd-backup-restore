21416b1d9af2ac79791573f0a5e457e32039417b
Ishan Tyagi <42602577+ishan16696@users.noreply.github.com>
2024-08-30 00:00:57 +0530

    Retry to take full snapshot if prev full snapshot fails due to some reason. (#765)
    * Retry to take full snapshot if prev full snapshot fails.
    
    * Retry with backoff.
    
    * Move checking inside func IsFullSnapshotRequiredAtStartup().
    
    * Fixed unit tests.
    
    * Added unit test.
    
    Address review comments.

--- pkg/snapshot/snapshotter/snapshotter_test.go
+++ pkg/snapshot/snapshotter/snapshotter_test.go
@@ -790,6 +790,22 @@ var _ = Describe("Snapshotter", func() {
 				})
 			})
 
+			Context("Previous full snapshot was not successful", func() {
+				It("should return true", func() {
+					snapshotterConfig := &brtypes.SnapshotterConfig{
+						FullSnapshotSchedule: fmt.Sprintf("%d %d * * *", (currentMin+1)%60, (currentHour+2)%24),
+					}
+
+					ssr, err = NewSnapshotter(logger, snapshotterConfig, store, etcdConnectionConfig, compressionConfig, healthConfig, snapstoreConfig)
+					Expect(err).ShouldNot(HaveOccurred())
+
+					// previous full snapshot wasn't successful
+					ssr.PrevFullSnapshotSucceeded = false
+					isFullSnapMissed := ssr.IsFullSnapshotRequiredAtStartup(fullSnapshotTimeWindow)
+					Expect(isFullSnapMissed).Should(BeTrue())
+				})
+			})
+
 			Context("Previous full snapshot was taken exactly at scheduled snapshot time, no FullSnapshot was missed", func() {
 				It("should return false", func() {
 					snapshotterConfig := &brtypes.SnapshotterConfig{
@@ -803,6 +819,7 @@ var _ = Describe("Snapshotter", func() {
 					ssr.PrevFullSnapshot = &brtypes.Snapshot{
 						CreatedOn: time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day()-1, (currentHour+2)%24, (currentMin+1)%60, 0, 0, time.Local),
 					}
+					ssr.PrevFullSnapshotSucceeded = true
 
 					isFullSnapMissed := ssr.IsFullSnapshotRequiredAtStartup(fullSnapshotTimeWindow)
 					Expect(isFullSnapMissed).Should(BeFalse())
@@ -823,6 +840,7 @@ var _ = Describe("Snapshotter", func() {
 					ssr.PrevFullSnapshot = &brtypes.Snapshot{
 						CreatedOn: time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), (currentHour-4)%24, (currentMin-10)%60, 0, 0, time.Local),
 					}
+					ssr.PrevFullSnapshotSucceeded = true
 					isFullSnapCanBeMissed := ssr.IsFullSnapshotRequiredAtStartup(fullSnapshotTimeWindow)
 					Expect(isFullSnapCanBeMissed).Should(BeFalse())
 				})
@@ -842,6 +860,7 @@ var _ = Describe("Snapshotter", func() {
 					ssr.PrevFullSnapshot = &brtypes.Snapshot{
 						CreatedOn: time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), (currentHour-18)%24, (currentMin)%60, 0, 0, time.Local),
 					}
+					ssr.PrevFullSnapshotSucceeded = true
 					isFullSnapCanBeMissed := ssr.IsFullSnapshotRequiredAtStartup(fullSnapshotTimeWindow)
 					Expect(isFullSnapCanBeMissed).Should(BeTrue())
 				})
