4eb79478c664d3b9e055be68fef6cffc19c36b7c
Saketh Kalaga <51327242+renormalize@users.noreply.github.com>
2024-10-16 10:01:36 +0530

    Compactor does not create the database directory anymore, since the Restore API does it.
    * Restoration through the `Restore` API will fail if a directory for the database is
      already present. Due to this, explicitly creating a directory before restoration is
      triggerred is not performed anymore. The `Restore` call creates the directory.

--- pkg/compactor/compactor.go
+++ pkg/compactor/compactor.go
@@ -7,7 +7,6 @@ package compactor
 import (
 	"context"
 	"fmt"
-	"os"
 	"time"
 
 	"github.com/gardener/etcd-backup-restore/pkg/compressor"
@@ -66,19 +65,6 @@ func (cp *Compactor) Compact(ctx context.Context, opts *brtypes.CompactOptions) 
 		cp.logger.Error("No base snapshot found. Nothing is available for compaction")
 		return nil, fmt.Errorf("no base snapshot found. Nothing is available for compaction")
 	}
-
-	cp.logger.Infof("Creating temporary etcd directory %s for restoration.", compactorRestoreOptions.Config.DataDir)
-	err := os.MkdirAll(compactorRestoreOptions.Config.DataDir, 0700)
-	if err != nil {
-		cp.logger.Errorf("Unable to create temporary etcd directory for compaction: %s", err.Error())
-		return nil, err
-	}
-
-	defer func() {
-		if err := os.RemoveAll(compactorRestoreOptions.Config.DataDir); err != nil {
-			cp.logger.Errorf("Failed to remove temporary etcd directory %s: %v", compactorRestoreOptions.Config.DataDir, err)
-		}
-	}()
 
 	// Then restore from the snapshots
 	r, err := restorer.NewRestorer(cp.store, cp.logger)
