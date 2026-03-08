0ee867b4d2552363334429f4713afff233596f9e
Shreyas S <shreyas.s@sap.com>
2026-03-09 00:37:57 +0530

    changed wrapper image to official one

--- hack/ci-e2e-kind.sh
+++ hack/ci-e2e-kind.sh
@@ -11,7 +11,7 @@ export ETCDBR_IMAGE="europe-docker.pkg.dev/gardener-project/public/gardener/etcd
 #TODO: (@Shreyas-s14) change the below image once etcd-wrapper is released.
 export ETCD_WRAPPER_IMAGE="europe-docker.pkg.dev/gardener-project/snapshots/gardener/etcd-wrapper"
 export ETCDBR_VERSION="dev-latest" # This version gets built from the current branch, loaded into the kind cluster and used for the e2e tests
-export ETCD_WRAPPER_VERSION="v0.7.0-da33bcc9befc2a6d81ea43ddb88c0b2b2cd26041"
+export ETCD_WRAPPER_VERSION="latest"
 
 TEST_PROVIDERS=${1:-"aws"}
 
