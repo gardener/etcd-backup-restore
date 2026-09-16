19ca1260ecc2cb99e4be697b95b7c17e6b7b4570
Ishan Tyagi <42602577+ishan16696@users.noreply.github.com>
2026-09-16 10:06:28 +0530

    Removes extra handling done to change the file-permissions of legacy `safe_guard` files. (#1071)
    Signed-off-by: ishan16696 <ishan.tyagi@sap.com>

--- pkg/initializer/validator/datavalidator.go
+++ pkg/initializer/validator/datavalidator.go
@@ -98,12 +98,6 @@ func (d *DataValidator) sanityCheck() (DataDirStatus, error) {
 			}
 		}
 
-		// Change file permissions of legacy `safe_guard` files.
-		// TODO: To be removed in etcd-backup-restore:v0.41.0.
-		if err := os.Chmod(path, 0600); err != nil {
-			d.Logger.Fatalf("can't change the permission of the `safe_guard` file because : %v", err)
-		}
-
 		// read the content of the file safe_guard and match it with the environment variable
 		content, err := os.ReadFile(path) // #nosec G304 -- this is a trusted safe_guard file written to by etcdbr.
 		if err != nil {
